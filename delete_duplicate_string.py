93d14775065e7b0885a22cb6577e4a55439c8f19
viktor borodin <vikbrdin@gmail.com>
2022-01-02 23:49:58 +0700

    Fix deleting timecodes

--- delete_duplicate_string.py
+++ delete_duplicate_string.py
@@ -5,12 +5,12 @@ import work_with_files as files
 
 
 def is_empty_line(line: str):
-    """Check if line is empty. Returnes True if line contains only whitespaces"""
+    """Check if line is empty. Returns True if line contains only whitespaces"""
     return bool(re.match(r"\s*\n", line))
 
 
 def is_time_code(line: str):
-    """Check if line has timecode. Returnes True if line contain timecode"""
+    """Check if line has timecode. Returns True if line contain timecode"""
     time = r"\d{2}:\d{2}:\d{2}\.\d{3}"
     return bool(re.match(time + r"\s*-->\s*" + time + r"\s*\n", line))
 
@@ -19,8 +19,23 @@ def is_suitable_line(line: str):
     return not is_empty_line(line) and not is_time_code(line)
 
 
+def get_chunk(text: list):
+    for index in reversed(range(1, len(text) - 1)):
+        yield index, text[index-1:index+2]
+
+
+def is_empty_timecode(chunk: list):
+    return is_empty_line(chunk[0]) and is_time_code(chunk[1]) and is_empty_line(chunk[2])
+
+
+def del_chunk_with_index(index: int, text: list):
+    text.pop(index + 1)
+    text.pop(index)
+    text.pop(index - 1)
+
+
 def delete_duplicate_lines(input_text: list) -> list:
-    """Take list with lines from read file and returnes list of lines without duplicates and extra timecodes
+    """Take list with lines from read file and returns list of lines without duplicates and extra timecodes
 
     Args:
         input_text (list): lines from the read file and processed with the help delete_service_data.py script
@@ -39,6 +54,12 @@ def delete_duplicate_lines(input_text: list) -> list:
                 found_lines.add(line)
             else:
                 output_text.pop(index)
+
+    for index, chunk in get_chunk(output_text):
+        print("index:", index)
+        print("chunk:", chunk)
+        if is_empty_timecode(chunk):
+            del_chunk_with_index(index, output_text)
 
     return output_text
 
